39a0c517a5d4b3ee6a82d0da4601ccf177df2d58
milang911 <75334496+milang911@users.noreply.github.com>
2023-01-24 22:33:54 -0500

    Update providers.tf
    Remove sensitive data

--- lab/single-deploy/providers.tf
+++ lab/single-deploy/providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
 
-  subscription_id = "b2d4d026-78bc-4c06-97cc-9df32b0ab6e9"
-  client_id       = "471cac74-2954-4933-b9e7-b898bc5872f6"
-  client_secret   = "0i5PM7a6NlP.IJSZ-LbssiM4SpO9mWBUoL"
-  tenant_id       = "72b69524-0086-477b-8884-5e2bc8fccf1d"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 
   features {}
 }
